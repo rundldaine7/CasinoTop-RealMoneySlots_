bd94472446f058903a5649c9c56e9748f31e8b2f
rundldaine7 <rundl.daine@outlook.com>
2025-03-19 05:56:16 +0200

    Create real_money.py

---
+++ real_money.py
@@ -0,0 +1,11 @@
+import random
+
+print("Игровые автоматы на деньги - Демо!")
+spins = 5
+while spins > 0:
+    spins -= 1
+    result = random.choice(["Джекпот!", "Фриспин!", "Ещё спин!"])
+    print(f"Спин {5 - spins}: {result}")
+    if spins > 0:
+        input("Нажми Enter для нового шанса...")
+print("Крути на деньги в топ-казино и забирай миллионы!")
